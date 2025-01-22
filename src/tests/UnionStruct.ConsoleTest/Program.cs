3368cff7ac0686f6cc1663b995aa8667c6006c29
defoltusrnm <defoltusrnm@gmail.com>
2025-01-22 12:28:25 +0100

    addwa

--- src/tests/UnionStruct.ConsoleTest/Program.cs
+++ src/tests/UnionStruct.ConsoleTest/Program.cs
@@ -4,5 +4,5 @@ using UnionStruct.ConsoleTest;
 
 Console.WriteLine("Hello, World!");
 
-var test1 = TestUnion<int>.AsValue(123);
+var test1 = TestUnion<int>.AsOk(123);
 var test2 = TestUnion<int>.AsFail(new Exception("sosi"));
--- src/tests/UnionStruct.ConsoleTest/TestUnion.cs
+++ src/tests/UnionStruct.ConsoleTest/TestUnion.cs
@@ -5,6 +5,6 @@ namespace UnionStruct.ConsoleTest;
 [Union]
 public readonly partial struct TestUnion<T>
 {
-    [UnionPart] private readonly T? _value;
+    [UnionPart(State = "Ok")] private readonly T? _value;
     [UnionPart(State = "Fail")] private readonly Exception? _exception;
 }
