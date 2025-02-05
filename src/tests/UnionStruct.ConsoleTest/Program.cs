97933efe846ffef899c76ed605e3d6202322cf93
defoltusrnm <defoltusrnm@gmail.com>
2025-02-05 12:13:42 +0100

    dawdwa

--- src/tests/UnionStruct.ConsoleTest/Program.cs
+++ src/tests/UnionStruct.ConsoleTest/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using UnionStruct.ConsoleTest;
+using UnionStruct.Unions;
 
 var option = await Option<DateTimeOffset>.Some(DateTimeOffset.UtcNow)
     .MapSome(async x =>
